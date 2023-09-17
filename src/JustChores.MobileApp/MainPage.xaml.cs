129d60e4b251578bcd53bc07ba60853e6d43634f
Neville Nazerane <neville@mediroutes.com>
2023-09-16 20:45:45 -0700

    setup

---
+++ src/JustChores.MobileApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace JustChores.MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                hello.Text = ex.Message;
+            }
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
