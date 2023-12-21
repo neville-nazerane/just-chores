1747b05becee2fb60b30acf7058440dbffa2a429
Neville Nazerane <live.neville@live.com>
2023-12-21 04:43:46 -0700

    setting up theme button component

---
+++ src/JustChores.MobileApp/Components/ThemeButtonComponent.xaml.cs
@@ -0,0 +1,109 @@
+
+
+
+namespace JustChores.MobileApp.Components;
+
+public partial class ThemeButtonComponent : Border
+{
+
+	#region BindableProperties
+
+	public static readonly BindableProperty TextProperty = BindableProperty.Create(
+																	nameof(Text),
+																	typeof(string),
+																	typeof(ThemeButtonComponent),
+																	defaultBindingMode: BindingMode.OneWay,
+																	propertyChanged: TextChanged);
+
+	public static readonly BindableProperty IconImageProperty = BindableProperty.Create(
+																					nameof(IconImage),
+																					typeof(string),
+																					typeof(ThemeButtonComponent),
+																					defaultBindingMode: BindingMode.OneWay,
+																					propertyChanged: IconImageChanged);
+
+	public static readonly BindableProperty SelectedThemeProperty = BindableProperty.Create(
+																						nameof(SelectedTheme),
+																						typeof(AppTheme),
+																						typeof(ThemeButtonComponent),
+																						defaultBindingMode: BindingMode.OneWay,
+																						propertyChanged: SelectedThemeChanged);
+
+	public static readonly BindableProperty SelfThemeProperty = BindableProperty.Create(
+																					nameof(SelfTheme),
+																					typeof(AppTheme),
+																					typeof(ThemeButtonComponent),
+																					defaultBindingMode: BindingMode.OneWay,
+																					propertyChanged: SelfThemeChanged);
+
+	#endregion
+
+	#region Properties
+
+	public string Text
+	{
+		get => (string)GetValue(TextProperty);
+		set => SetValue(TextProperty, value);
+	}
+
+	public string IconImage
+	{
+		get => (string)GetValue(IconImageProperty);
+		set => SetValue(IconImageProperty, value);
+	}
+
+	public AppTheme SelectedTheme
+	{
+		get => (AppTheme)GetValue(SelectedThemeProperty);
+		set => SetValue(SelectedThemeProperty, value);
+	}
+
+    public AppTheme SelfTheme
+    {
+        get => (AppTheme)GetValue(SelfThemeProperty);
+        set => SetValue(SelfThemeProperty, value);
+    }
+
+    #endregion
+
+
+    public ThemeButtonComponent()
+	{
+		InitializeComponent();
+	}
+
+	void SetText(string text)
+	{
+		titleLbl.Text = text;
+	}
+
+	void SetIconImage(string image)
+	{
+		icon.Source = ImageSource.FromResource(image);
+	}
+
+    private void SetSelectedTheme(AppTheme newValue)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SetSelfTheme(AppTheme newValue)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    #region Changed
+
+    private static void TextChanged(BindableObject bindable, object oldValue, object newValue) => ((ThemeButtonComponent)bindable).SetText((string)newValue);
+
+	private static void IconImageChanged(BindableObject bindable, object oldValue, object newValue) => ((ThemeButtonComponent)bindable).SetIconImage((string)newValue);
+
+	private static void SelectedThemeChanged(BindableObject bindable, object oldValue, object newValue) => ((ThemeButtonComponent)bindable).SetSelectedTheme((AppTheme)newValue);
+
+    private static void SelfThemeChanged(BindableObject bindable, object oldValue, object newValue) => ((ThemeButtonComponent)bindable).SetSelfTheme((AppTheme)newValue);
+
+
+    #endregion
+
+}
